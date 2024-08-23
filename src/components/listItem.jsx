f17b3a55cf9d848a5a08d359ad6307086a1d4d5c
cnadityasai <cnadityasai@gmail.com>
2024-08-23 17:11:10 +0100

    add text cut when complete

--- src/components/listItem.jsx
+++ src/components/listItem.jsx
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./listItem.css";
 
-const listItem = (props) => {
+const ListItem = (props) => {
   const title = props.title;
   const id = props.id;
+  const [check, setCheck] = useState(false);
+
+  function handleChange() {
+    setCheck(!check);
+  }
+
   return (
     <div class="item">
-      <input type="checkbox" id={id}></input>
-      <label htmlFor={id}>{title}</label>
+      <input
+        type="checkbox"
+        id={id}
+        checked={check}
+        onChange={handleChange}
+      ></input>
+      <label
+        style={{ textDecoration: check ? "line-through" : "None" }}
+        htmlFor={id}
+      >
+        {title}
+      </label>
     </div>
   );
 };
 
-export default listItem;
+export default ListItem;
